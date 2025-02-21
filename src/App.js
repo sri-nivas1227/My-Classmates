0eeeeec9594e4b9c0a8db83b68f0533b678fda2d
sri-nivas1227 <srinivasmekala1227@gmail.com>
2025-02-21 16:24:27 -0500

    static people data added as a list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const people = [
+    { name: "Varshith Bachu", favoriteColor: "red", favoriteFood: "Mandi" },
+    {
+      name: "Aakanksha Rangdal",
+      favoriteColor: "Lavendar",
+      favoriteFood: "ice-cream",
+    },
+    { name: "Bharat Vamsy", favoriteColor: "blue", favoriteFood: "Biryani" },
+    {
+      name: "Srinivas Mekala",
+      favoriteColor: "sky blue",
+      favoriteFood: "Dosa",
+    },
+  ];
   return (
     <div className="App">
       <header className="App-header">
