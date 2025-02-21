c4f9570422d02b29c83772c8ecbbbcec3bd81d50
sri-nivas1227 <srinivasmekala1227@gmail.com>
2025-02-21 16:28:20 -0500

    rendering the people cards inside a bootstrap container and arranged the cards in a flex box

--- src/App.js
+++ src/App.js
@@ -17,22 +17,13 @@ function App() {
     },
   ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="my-4">
+      <Container className="w-50 d-flex flex-column gap-3">
+        {people.map((person, index) => (
+          <Card key={index} person={person} />
+        ))}
+      </Container>
+    </Container>
   );
 }
 
