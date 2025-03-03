f1d166a5901ba5bd0e8c26bc83f628b0de7b7342
sri-nivas1227 <srinivasmekala1227@gmail.com>
2025-03-02 20:20:14 -0500

    modular code, table and tile view, DOK level 3

---
+++ src/DataGridDemo.jsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { DataGrid } from "@mui/x-data-grid";
+
+const columns = [
+  { field: "name", headerName: "Name", width: 150 },
+  {
+    field: "favoriteColor",
+    headerName: "Favorite Color",
+    width: 150,
+    // editable: true,
+  },
+  {
+    field: "favoriteFood",
+    headerName: "Favorite Food",
+    width: 150,
+    // editable: true,
+  },
+];
+
+export default function DataGridDemo({ peopleData }) {
+  return (
+    <div className="m-2 w-50">
+      <Box sx={{ height: 400, width: "100%", backgroundColor: "white" }}>
+        <DataGrid
+          rows={peopleData}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </Box>
+    </div>
+  );
+}
